3fb2b83510d703c15827595f0995a097afa1b114
Jackson Hughes <jxhughes95@gmail.com>
2020-02-08 23:31:45 +0000

    Move classes to hiera

--- manifests/site.pp
+++ manifests/site.pp
@@ -28,6 +28,6 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  class { 'linuxusers': }
-  class { 'puppetcron': }
 }
+
+lookup('classes', Array[String], 'unique').include
