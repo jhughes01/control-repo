4e053872ab2133b1ce2d0599a6f8249619ed4569
Jackson Hughes <jxhughes95@gmail.com>
2020-02-09 23:14:07 +0000

    disable troublemaker

--- site-modules/profile/manifests/jenkins.pp
+++ site-modules/profile/manifests/jenkins.pp
@@ -1,12 +1,12 @@
 # Site specific Jenkins config
 class profile::jenkins {
-  jenkins::plugin { 'mailer': }
-  jenkins::plugin { 'display-url-api version':
-    version => '2.3.2',
-  }
+  # jenkins::plugin { 'mailer': }
+  # jenkins::plugin { 'display-url-api version':
+  #   version => '2.3.2',
+  # }
 
-  jenkins::user { 'admin':
-    email    => 'admin@fake.com',
-    password => 'changeme',
-  }
+  # jenkins::user { 'admin':
+  #   email    => 'admin@fake.com',
+  #   password => 'changeme',
+  # }
 }
