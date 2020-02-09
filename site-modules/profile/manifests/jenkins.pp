68fae3afd19317436aa4b787d16f044c1077b661
Jackson Hughes <jxhughes95@gmail.com>
2020-02-09 22:59:16 +0000

    add jenkins plugin

--- site-modules/profile/manifests/jenkins.pp
+++ site-modules/profile/manifests/jenkins.pp
@@ -1,6 +1,7 @@
 # Site specific Jenkins config
 class profile::jenkins {
   jenkins::plugin { 'mailer': }
+  jenkins::plugin { 'display-url-api version': }
 
   jenkins::user { 'admin':
     email    => 'admin@fake.com',
