924eefec9bd4d91235a9eed456e9f8c2c31aa1b7
Jackson Hughes <jxhughes95@gmail.com>
2020-02-09 18:41:52 +0000

    change from r10k to librarian puppet

--- scripts/run-puppet.sh
+++ scripts/run-puppet.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-cd /etc/puppetlabs/code/environments/production && git pull
-/opt/puppetlabs/puppet/bin/r10k puppetfile install
+CODE_DIR="/etc/puppetlabs/code/environments/production"
+
+rm -rf "$CODE_DIR" && git clone https://github.com/jhughes01/control-repo "$CODE_DIR"
+cd "$CODE_DIR"
+/opt/puppetlabs/puppet/bin/librarian-puppet install
 /opt/puppetlabs/bin/puppet apply /etc/puppetlabs/code/environments/production/manifests/site.pp
