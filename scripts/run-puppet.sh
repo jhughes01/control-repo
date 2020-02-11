bcaa2b970ccdccfcfd68b376c443b51478d5d79d
Jackson Hughes <jxhughes95@gmail.com>
2020-02-11 21:59:51 +0000

    Update run puppet script to use r10k

--- scripts/run-puppet.sh
+++ scripts/run-puppet.sh
@@ -4,5 +4,5 @@ CODE_DIR="/etc/puppetlabs/code/environments/production"
 
 rm -rf "$CODE_DIR" && git clone https://github.com/jhughes01/control-repo "$CODE_DIR"
 cd "$CODE_DIR"
-/opt/puppetlabs/puppet/bin/librarian-puppet install
+/opt/puppetlabs/puppet/bin/r10k puppetfile install --verbose
 /opt/puppetlabs/bin/puppet apply /etc/puppetlabs/code/environments/production/manifests/site.pp
