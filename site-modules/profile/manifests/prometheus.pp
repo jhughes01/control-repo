330216b7a801b9ee8e9c13ee0951aba6b64ce393
Jackson Hughes <jxhughes95@gmail.com>
2020-02-13 23:24:23 +0000

    .

--- site-modules/profile/manifests/prometheus.pp
+++ site-modules/profile/manifests/prometheus.pp
@@ -1,10 +1,19 @@
 class profile::prometheus {
-  include prometheus
-
-  scrape_configs [
+  scrape_configs           => [
+    {
+      'job_name'        => 'prometheus',
+      'scrape_interval' => '10s',
+      'scrape_timeout'  => '10s',
+      'static_configs'  => [
+        {
+          'targets' => ['localhost:9090'],
+          'labels'  => {'alias' => 'Prometheus'}
+        }
+      ],
+    },
     {
       'job_name'        => 'node',
-      'scrape_interval' => '15s',
+      'scrape_interval' => '5s',
       'scrape_timeout'  => '5s',
       'static_configs'  => [
         {
@@ -13,5 +22,5 @@ class profile::prometheus {
         },
       ],
     },
-  ]
+  ],
 }
