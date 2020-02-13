1df7825d56838c0504ad0a2b5b4bfd478c279d7b
Jackson Hughes <jxhughes95@gmail.com>
2020-02-13 23:18:57 +0000

    Add node exporter config

--- site-modules/profile/manifests/prometheus.pp
+++ site-modules/profile/manifests/prometheus.pp
@@ -1,3 +1,17 @@
 class profile::prometheus {
   include prometheus
+
+  scrape_configs [
+    {
+      'job_name'        => 'node',
+      'scrape_interval' => '15s',
+      'scrape_timeout'  => '5s',
+      'static_configs'  => [
+        {
+          'targets' => ['nodexporter.domain.com:9100'],
+          'labels'  => {'alias' => 'Node'}
+        },
+      ],
+    },
+  ]
 }
