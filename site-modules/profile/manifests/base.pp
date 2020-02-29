f40fd027987523c39fb392ee42a9975c90396c9a
Jackson Hughes <jxhughes95@gmail.com>
2020-02-29 22:04:59 +0000

    Modify base profile

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -1,6 +1,5 @@
 # base profile for common classes to be applied to all nodes
 class profile::base {
-  include linuxusers
   include puppetcron
   include firewalld
   include prometheus::node_exporter
