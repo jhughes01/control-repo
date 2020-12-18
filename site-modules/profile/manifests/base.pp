c121b9b75663ee1c8548327ad345cdaf977dcd98
Jackson Hughes <jxhughes95@gmail.com>
2020-12-18 00:24:49 +0000

    fix

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -18,6 +18,10 @@ class profile::base {
     ],
   }
 
+  file { '/etc/puppetlabs/r10k':
+    ensure => 'directory'
+  }
+
   file { '/etc/puppetlabs/r10k/r10k.yaml.':
     ensure => 'file',
     source => 'puppet:///modules/profile/r10k.yaml'
